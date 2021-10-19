b9a0e3d3f484966b578118ce4b4dd1d4c368f688
Cynthia <amirah.julia@gmail.com>
2021-10-19 21:36:27 +0200

    testy skonczone

--- Lab1/books.py
+++ Lab1/books.py
@@ -6,43 +6,57 @@ def list(dict):
     for arg in dict:
         print(arg, dict[arg])
     
-def error_warn()
-    return "Taka ksiazka nie istnieje"
+
+def Zwrot(data, users, books):
+    if data[1] in users[data[0]]:
+        books[data[1]] = books[data[1]] + 1
+        users[data[0]].remove(data[1])
+    else:
+        warnings.warn("Uzytkownik nie moze zwrocic ksiazki ktorej nie posiada")
+        return None
 
 
-books = dict([arg.split(':') for arg in sys.argv[1:]])
+def Wypozyczenie(data, users, books):
+    if data[1] not in books:
+        print("Dana ksiazka nie istnieje")
+        return False
+    elif int(books[data[1]]) > 0:
+        users[data[0]].append(data[1])
+        books[data[1]] -= 1
+    else:
+        print("Ksiazka niedostepna")
+        return None
 
-for arg in books:
-    books[arg] = int(books[arg])
+
+def dictionary(args):
+    a = dict([arg.split(':') for arg in args])
+    for arg in a:
+        a[arg] = int(a[arg])
+    return a
+
+
+books = dictionary(sys.argv[1:])
+
 
 users = {}
 
-while True:
-    try:
-        usr_inp = input()
-        if chr(4) in usr_inp:
+if __name__ == "__main__":
+    while True:
+        try:
+            usr_inp = input()
+            if chr(4) in usr_inp:
+                break
+            if usr_inp:
+                data = usr_inp.split(' ')
+            if data[0] not in users:
+                users[data[0]] = []
+            if data[2] == "Z":  #obsluga zwrotow
+                Zwrot(data, users, books)
+            elif data[2] == "W":   #obsluga wypozyczenia
+                Wypozyczenie(data, users, books)
+        except EOFError:
             break
-        if usr_inp:
-            data = usr_inp.split(' ')
-        if data[0] not in users:
-            users[data[0]] = []
 
-        if data[2] == "Z":  #obsluga zwrotow
-            if data[1] in users[data[0]]:
-                books[data[1]] = books[data[1]] + 1
-                users[data[0]].remove(data[1])
-            else:
-                warnings.warn("Uzytkownik nie moze zwrocic ksiazki ktorej nie posiada")
-        elif data[2] == "W":   #obsluga wypozyczenia
-            if data[1] not in books:
-                priny(error_warn())
-            elif int(books[data[1]]) > 0:
-                users[data[0]].append(data[1])
-                books[data[1]] -= 1
-            else:
-                print("Ksiazka niedostepna")
-    except EOFError:
-        break
 
 list(books)
 for arg in users:
--- Lab1/books_test.py
+++ Lab1/books_test.py
@@ -5,19 +5,18 @@ import sys
 
 
 class Test_TestSum(unittest.TestCase):
-    def test_warning(self):
-        with unittest.mock.patch(sys.argv, ["books.py", "Ksiazka:3", "Ksiazka2:4"]):
-            for arg in books:
-                print(arg, books[arg])
-            self.assertWarns(UserWarning)
+    def test_dictionary(self):
+        self.assertEqual(books.dictionary(["Ksiazka:3", "Ksiazka2:4"]), {"Ksiazka": 3, "Ksiazka2": 4})
+    
+    def test_rent_warning(self):
+        self.assertEqual(books.Wypozyczenie(["Adam", "Ksiazka", "W"], {"Adam": ["Ksiazka", "Ksiazka2"]}, {"Ksiazka": 0, "Ksiazka2": 3}), None)
 
+    def test_dictionary_error(self):
+        with self.assertRaises(Exception):
+            books.dictionary(["Ksiazka:XD", "Ksiazka2:4"])
+    
+    def test_book_return(self):
+        self.assertEqual(books.Zwrot(["Adam", "Ksiazka", "Z"], {"Adam": ["Ksiazka2"]}, {"Ksiazka": 0, "Ksiazka2": 3}), None)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--input', default='Ksiazka:2')
-    parser.add_argument('unittest_args', nargs='*')
-
-    args = parser.parse_args()
-
-    sys.argv[1:] = args.unittest_args
     unittest.main()
